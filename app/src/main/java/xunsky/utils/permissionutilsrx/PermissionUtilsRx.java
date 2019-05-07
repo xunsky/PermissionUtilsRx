8cc9de1709c7a4270f334e9288a45800bb556b1a
weixun.lwx <weixun.lwx@ruishanio.com>
2019-05-07 11:48:01 +0800

    add check permanentlyreject

--- app/src/main/java/xunsky/utils/permissionutilsrx/PermissionUtilsRx.java
+++ app/src/main/java/xunsky/utils/permissionutilsrx/PermissionUtilsRx.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,7 +14,7 @@ import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
 
 public class PermissionUtilsRx {
-    public static void requestPermissions(AppCompatActivity act, final CallBack callBack, String... permissions){
+    public static void requestPermissions(AppCompatActivity act, final CallBack callBack, String... permissions) {
         new RxPermissions(act)
                 .request(permissions)
                 .subscribe(new Consumer<Boolean>() {
@@ -35,7 +36,7 @@ public class PermissionUtilsRx {
 
     //在Fragment中申请权限时不要使用fragment.getActivity()
     //有可能会导致java.lang.IllegalStateException: FragmentManager is already executing transactions.
-    public static void requestPermissions(Fragment fragment, final CallBack callBack, String... permissions){
+    public static void requestPermissions(Fragment fragment, final CallBack callBack, String... permissions) {
         new RxPermissions(fragment)
                 .request(permissions)
                 .subscribe(new Consumer<Boolean>() {
@@ -55,7 +56,8 @@ public class PermissionUtilsRx {
                 });
 
     }
-    public static boolean checkPermission(AppCompatActivity act, String... permissions){
+
+    public static boolean checkPermission(AppCompatActivity act, String... permissions) {
 //        Observable.just(1)
 //                .compose(
 //                        new RxPermissions(act)
@@ -77,8 +79,23 @@ public class PermissionUtilsRx {
         }
         return true;
     }
-    public interface CallBack{
+
+    public static boolean haveSomePermissionPermanentlyDenied(Activity act, String... permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return false;
+
+        for (int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            boolean isPermanentlyReject = !ActivityCompat.shouldShowRequestPermissionRationale(act, permission);
+            if (isPermanentlyReject)
+                return true;
+        }
+        return false;
+    }
+
+    public interface CallBack {
         void success();
+
         void fail(String message);
     }
 }
